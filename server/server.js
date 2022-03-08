34e76e9620d5c61d6b36c553eab2655a4b9668e6
JasonWoo665 <jasonwoo665@gmail.com>
2022-03-08 14:45:10 +0800

    register and login system implmented

--- server/server.js
+++ server/server.js
@@ -10,11 +10,27 @@ const io = require('socket.io')(server, {
         origin : "*",
     },
 })
+// for register usage
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const User = require('../model/user')
+const bcrypt = require('bcrypt')
+mongoose.connect('mongodb://localhost:27017/local-api-register', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+// for login usage
+const jwt = require('jsonwebtoken')
+const JWT_SECRET = '32pvutywoh#cgiwao(&%^$U#Y@;qrfhoq3pfo72 39fj,gp90tu004yb445jdqadpso||alsavmvkljoetw[qpavmknhug'
 
+// for socket io identification
 let clientList = [] 
 let dataList = []
 const users = {} // just for chat room server side, may merge with clientList later for simplicity
 
+// for register usage
+app.use(bodyParser.json());
+// set paths
 app.use("/src", express.static('../src/'));
 app.use("/indexScript", express.static('../indexScript/'));
 app.use("/locateFile", express.static('../locateFile/'));
@@ -26,7 +42,53 @@ app.set("views", "../")
 app.get('/', (req, res) => {
     // res.sendFile('simple.html', { root: '../' })
     res.render('index',{ title: 'Chat room '+req.id})
+    console.dir("coming id: ",req.id)
 });
+app.get('/register', (req, res) => {
+    res.render('register',{ title: 'Register for '+req.id})
+});
+app.get('/login', (req, res) => {
+    res.render('login',{ title: 'login for '+req.id})
+});
+// handles login stuff
+app.post('/api/login', async (req, res)=>{
+    const {username, password} = req.body
+    console.log(username, password)
+    const user =  await User.findOne({ username }).lean()
+    if (!user){
+        return res.json({status: 'error', error: 'Invalid username/password'})
+    }
+    if (await bcrypt.compare(password, user.password)){ 
+        const token = jwt.sign({
+            id: user._id, 
+            username: user.username
+        }, JWT_SECRET)
+
+        return res.json({status: 'ok', data: token})
+    }
+    res.json({status: 'error', error: 'Invalid username/password'})
+    // res.json({status: 'ok', data: `${username} : ${password}`})
+})
+
+// handles register stuff
+app.post('/api/register', async (req, res)=>{
+    const {username, password: plainTextPassword} = req.body
+    const password = await bcrypt.hash(plainTextPassword,10)
+    try{
+        const response = await User.create({
+            username,
+            password
+        })
+        res.json({status: 'ok'})
+        console.log('User created successfully:', response)
+    }catch(error){
+        if (error.code === 11000){ //duplicated key
+            return res.json({status: 'error', error: 'Username already in use'})
+        }
+        throw error
+    }
+
+})
 
 io.on('connection', (socket) => {
     // update the user list when new usesr connects
